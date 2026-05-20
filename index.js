90eb9ea2c0354eb7a67bd83cf80f94f982f841f0
abeabeweiss-crypto <abeabeweiss@gmail.com>
2026-05-19 23:41:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const axios = require('axios');
 const app = express();
 app.use(express.json());
 const CLICKUP_API_KEY = 'pk_108004485_WP9UM93M00FT2X1Z6T5H4TUE9LBWVTHL';
@@ -14,30 +13,20 @@ const DESTINATION_FOLDER = 'Upstate Estimates';
 async function getMicrosoftToken() {
 const r = await fetch(https://login.microsoftonline.com/${AZURE_TENANT_ID}/oauth2/v2.0/token, {
 method: 'POST',
-body: new URLSearchParams({
-client_id: AZURE_CLIENT_ID,
-client_secret: AZURE_CLIENT_SECRET,
-scope: 'https://graph.microsoft.com/.default',
-grant_type: 'client_credentials'
-})
+headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+body: new URLSearchParams({ client_id: AZURE_CLIENT_ID, client_secret: AZURE_CLIENT_SECRET, scope: 'https://graph.microsoft.com/.default', grant_type: 'client_credentials' }).toString()
 });
 const d = await r.json();
 return d.access_token;
 }
 async function getClickUpTask(taskId) {
-const r = await fetch(https://api.clickup.com/api/v2/task/${taskId}, {
-headers: { Authorization: CLICKUP_API_KEY }
-});
+const r = await fetch(https://api.clickup.com/api/v2/task/${taskId}, { headers: { Authorization: CLICKUP_API_KEY } });
 return r.json();
 }
 async function copyFileInOneDrive(token, customerName) {
-const t = await fetch(https://graph.microsoft.com/v1.0/users/${ONEDRIVE_USER_EMAIL}/drive/root:/${TEMPLATE_FOLDER}/${TEMPLATE_FILE_NAME}, {
-headers: { Authorization: Bearer ${token} }
-});
+const t = await fetch(https://graph.microsoft.com/v1.0/users/${ONEDRIVE_USER_EMAIL}/drive/root:/${TEMPLATE_FOLDER}/${TEMPLATE_FILE_NAME}, { headers: { Authorization: Bearer ${token} } });
 const td = await t.json();
-const f = await fetch(https://graph.microsoft.com/v1.0/users/${ONEDRIVE_USER_EMAIL}/drive/root:/${DESTINATION_FOLDER}, {
-headers: { Authorization: Bearer ${token} }
-});
+const f = await fetch(https://graph.microsoft.com/v1.0/users/${ONEDRIVE_USER_EMAIL}/drive/root:/${DESTINATION_FOLDER}, { headers: { Authorization: Bearer ${token} } });
 const fd = await f.json();
 await fetch(https://graph.microsoft.com/v1.0/users/${ONEDRIVE_USER_EMAIL}/drive/items/${td.id}/copy, {
 method: 'POST',
@@ -45,9 +34,7 @@ headers: { Authorization: Bearer ${token}, 'Content-Type': 'application/json' },
 body: JSON.stringify({ name: ${customerName}.xlsx, parentReference: { driveId: td.parentReference.driveId, id: fd.id } })
 });
 await new Promise(r => setTimeout(r, 4000));
-const nf = await fetch(https://graph.microsoft.com/v1.0/users/${ONEDRIVE_USER_EMAIL}/drive/root:/${DESTINATION_FOLDER}/${customerName}.xlsx, {
-headers: { Authorization: Bearer ${token} }
-});
+const nf = await fetch(https://graph.microsoft.com/v1.0/users/${ONEDRIVE_USER_EMAIL}/drive/root:/${DESTINATION_FOLDER}/${customerName}.xlsx, { headers: { Authorization: Bearer ${token} } });
 const nfd = await nf.json();
 return nfd.webUrl;
 }
